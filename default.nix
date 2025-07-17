471fea2004bde625f14b75b009924b8c529cbff6
Annika Hannig <annika@hannig.cc>
2025-07-17 20:17:32 +0200

    initial flake

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "lightwatcher";
+  version = "3.0.0";
+  
+  src = ./.;
+  
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+  
+  meta = with pkgs.lib; {
+    description = "A lightweight clone of birdwatcher";
+    homepage = "https://github.com/alice-lg/lightwatcher";
+    license = licenses.mit;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Lightwatcher - A lightweight replacement for birdwatcher";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.callPackage ./default.nix { };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            rust-analyzer
+            clippy
+            
+            # Dev tools
+            cargo-watch
+            bacon
+            
+            # For testing with BIRD
+            bird
+          ];
+          
+          RUST_LOG = "info";
+          RUST_BACKTRACE = 1;
+        };
+      });
+}
